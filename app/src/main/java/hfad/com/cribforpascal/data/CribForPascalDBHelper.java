0753f5663d2ff59a01251da9ec2cb253659331ec
Vrabie Dmitrii <vrabiedmitrii@gmail.com>
2017-03-10 15:27:54 +0200

    Upgraded Database to V.2

--- app/src/main/java/hfad/com/cribforpascal/data/CribForPascalDBHelper.java
+++ app/src/main/java/hfad/com/cribforpascal/data/CribForPascalDBHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CribForPascalDBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "crib.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public CribForPascalDBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
